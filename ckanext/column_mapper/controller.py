dae3bd06759c38d8aa1ad5da63138e2420a6c47b
saravanan-blsk <saravanan@nineleaps.com>
2017-02-21 16:41:26 +0530

    Adds forms for column name edit

--- ckanext/column_mapper/controller.py
+++ ckanext/column_mapper/controller.py
@@ -4,12 +4,17 @@ import ckan.logic as logic
 import ckan.lib.base as base
 import ckan.plugins as p
 import ckan.lib.render
+import ckan.lib.helpers as h
+from ckan.common import OrderedDict, _, json, request, c, g, response
+import ckan.lib.render
 
 render = base.render
+abort = base.abort
+redirect = base.redirect
 
 
 class CMController(BaseController):
 
     def home(self):
         """Home page for column name mapper."""
-        return p.toolkit.render("home.html")
+        return p.toolkit.render("form/edit_mapping.html")
