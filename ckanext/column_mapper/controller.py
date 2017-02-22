adb372e034410a1b40a07ee8c74a2ce3907d766c
saravanan-blsk <saravanan@nineleaps.com>
2017-02-22 20:09:54 +0530

    WIP:Removing null entries

--- ckanext/column_mapper/controller.py
+++ ckanext/column_mapper/controller.py
@@ -39,8 +39,8 @@ class CMController(BaseController):
 
         # Retrieving resource data
         resource_id_dict = {'resource_id': id+'_mapping'}
-        pkg_data_dictionary = get_action('datastore_search')(context, resource_id_dict)
-        c.pkg_data_dictionary = pkg_data_dictionary['records']
+        pkg_data = get_action('datastore_search')(context, resource_id_dict)
+        c.pkg_data = pkg_data['records']
 
         # Setting Form action URL
         c.link = str("/dataset/column-mapper/update/" + id)
@@ -53,11 +53,45 @@ class CMController(BaseController):
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'auth_user_obj': c.userobj}
 
-        print '-------------------------------------------------------------------------'
-        print dir(request.copy_body)
-        print '-------------------------------------------------------------------------'
 
+        self.create_mapping_table(id, request, context)
+        # delete_data = {'resource_id': resource_id, 'force': True}
+        # try:
+        #     get_action('datastore_delete')(context, delete_data)
+        # except AttributeError:
+        #     pass
 
-        #TODO: Remove the counter from fields in edit_mapping.html file and get the request as a list.
+        redirect(h.url_for(controller='ckanext.column_mapper.controller:CMController',
+                           action='home', id=id))
 
-        return p.toolkit.render("form/edit_mapping.html")
+    def create_mapping_table(self, id, request, context):
+        """
+        Create mapping table.
+        :param id: str , Id of the resource
+        :param request: request Object
+        :param context: dict, Context
+        """
+
+        resource_id = id + "_mapping"
+        counter = 0
+        loop_controller = True
+
+        while loop_controller:
+            var_names = ['original_name_' + str(counter), 'mapped_name_' + str(counter), 'column_type_' + str(counter)]
+            original_name = request.params.get(var_names[0])
+            mapped_name = request.params.get(var_names[1])
+            column_type = request.params.get(var_names[2])
+
+            table_data = {'resource_id': resource_id, 'records': [{'original_name': original_name,
+                                                                   'mapped_name': mapped_name,
+                                                                   'column_type': column_type,
+                                                                   'resource_id': id}],
+                          'method': 'upsert', 'force': True}
+            print table_data
+            print
+            if original_name is not None or mapped_name is not None or column_type is not None:
+                get_action('datastore_upsert')(context, table_data)
+            else:
+                loop_controller = False
+
+            counter += 1
