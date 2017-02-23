6a4f159091cddbcea0b7959e365d4ebaf7c1ab99
saravanan-blsk <saravanan@nineleaps.com>
2017-02-23 12:43:49 +0530

    Code refactor

--- ckanext/column_mapper/controller.py
+++ ckanext/column_mapper/controller.py
@@ -1,13 +1,12 @@
 """Controller for the Column name mapping."""
-from ckan.lib.base import BaseController
-import ckan.model as model
-import ckan.logic as logic
 import ckan.lib.base as base
-import ckan.plugins as p
-import ckan.lib.render
 import ckan.lib.helpers as h
-from ckan.common import OrderedDict, _, json, request, c, g, response
 import ckan.lib.render
+import ckan.logic as logic
+import ckan.model as model
+import ckan.plugins as p
+from ckan.common import request, c
+from ckan.lib.base import BaseController
 
 render = base.render
 abort = base.abort
@@ -32,17 +31,20 @@ class CMController(BaseController):
     def home(self, id, data=None, errors=None):
         """Home page for column name mapper."""
 
-        # Creating context
+        # Create context
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'for_view': True,
                    'auth_user_obj': c.userobj, 'use_cache': False}
 
-        # Retrieving resource data
+        # Retrieve resource data
         resource_id_dict = {'resource_id': id+'_mapping'}
-        pkg_data = get_action('datastore_search')(context, resource_id_dict)
-        c.pkg_data = pkg_data['records']
+        try:
+            pkg_data = get_action('datastore_search')(context, resource_id_dict)
+            c.pkg_data = pkg_data['records']
+        except Exception:
+            pass
 
-        # Setting Form action URL
+        # Set Form action URL
         c.link = str("/dataset/column-mapper/update/" + id)
 
         return p.toolkit.render("form/edit_mapping.html")
@@ -53,20 +55,14 @@ class CMController(BaseController):
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'auth_user_obj': c.userobj}
 
-
-        self.create_mapping_table(id, request, context)
-        # delete_data = {'resource_id': resource_id, 'force': True}
-        # try:
-        #     get_action('datastore_delete')(context, delete_data)
-        # except AttributeError:
-        #     pass
+        self.upsert_mapping_data(id, request, context)
 
         redirect(h.url_for(controller='ckanext.column_mapper.controller:CMController',
                            action='home', id=id))
 
-    def create_mapping_table(self, id, request, context):
+    def upsert_mapping_data(self, id, request, context):
         """
-        Create mapping table.
+        Upsert mapping data into the table.
         :param id: str , Id of the resource
         :param request: request Object
         :param context: dict, Context
@@ -89,7 +85,7 @@ class CMController(BaseController):
                           'method': 'upsert', 'force': True}
             print table_data
             print
-            if original_name is not None or mapped_name is not None or column_type is not None:
+            if original_name or mapped_name or column_type:
                 get_action('datastore_upsert')(context, table_data)
             else:
                 loop_controller = False
